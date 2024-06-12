02fe6f5c58f676afbbde52db52e9620f6d820759
Mark Speich <mspeich@gmail.com>
2024-06-12 13:26:17 -0500

    Add isDone

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,8 +10,9 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
+      isDone: a.boolean(),
     })
-    .authorization((allow) => [allow.publicApiKey()]),
+    .authorization((allow) => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
--- src/App.tsx
+++ src/App.tsx
@@ -25,13 +25,14 @@ function App() {
 
   return (
     <Authenticator>
-      {({ signOut }) => (
+      {({ signOut, user }) => (
         <main>
+          <h1>{user?.signInDetails?.loginId}'s todos</h1>
           <h1>My todos</h1>
           <button onClick={createTodo}>+ new</button>
           <ul>
             {todos.map((todo) => (
-              <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
+              <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}, Done: {todo.isDone ? "Yes" : "No"}</li>
             ))}
           </ul>
           <div>
